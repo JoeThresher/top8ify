73849df5aab175301fb8a35de773b3518dc09158
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-11-10 20:17:22 -0500

    Added dynamic character image rendering

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -3,23 +3,56 @@ import { ref } from 'vue';
 import CharacterSelect from './CharacterSelect.vue';
 import OutputScreen from './OutputScreen.vue';
 
-const characters = defineModel<string[]>('characters', {
-  default: () => Array(8).fill(''),
-});
+const characters = ref<string[]>(Array(8).fill(''));
+const playerNames = ref<string[]>(Array(8).fill(''));
 </script>
 
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
-    <CharacterSelect playerNumber="1" v-model:selectedCharacter="characters[0]" />
-    <CharacterSelect playerNumber="2" v-model:selectedCharacter="characters[1]" />
-    <CharacterSelect playerNumber="3" v-model:selectedCharacter="characters[2]" />
-    <CharacterSelect playerNumber="4" v-model:selectedCharacter="characters[3]" />
-    <CharacterSelect playerNumber="5" v-model:selectedCharacter="characters[4]" />
-    <CharacterSelect playerNumber="6" v-model:selectedCharacter="characters[5]" />
-    <CharacterSelect playerNumber="7" v-model:selectedCharacter="characters[6]" />
-    <CharacterSelect playerNumber="8" v-model:selectedCharacter="characters[7]" />
+    <CharacterSelect
+      playerNumber="1"
+      v-model:selectedCharacter="characters[0]"
+      v-model:playerName="playerNames[0]"
+    />
+    <CharacterSelect
+      playerNumber="2"
+      v-model:selectedCharacter="characters[1]"
+      v-model:playerName="playerNames[1]"
+    />
+    <CharacterSelect
+      playerNumber="3"
+      v-model:selectedCharacter="characters[2]"
+      v-model:playerName="playerNames[2]"
+    />
+    <CharacterSelect
+      playerNumber="4"
+      v-model:selectedCharacter="characters[3]"
+      v-model:playerName="playerNames[3]"
+    />
+    <CharacterSelect
+      playerNumber="5"
+      v-model:selectedCharacter="characters[4]"
+      v-model:playerName="playerNames[4]"
+    />
+    <CharacterSelect
+      playerNumber="6"
+      v-model:selectedCharacter="characters[5]"
+      v-model:playerName="playerNames[5]"
+    />
+    <CharacterSelect
+      playerNumber="7"
+      v-model:selectedCharacter="characters[6]"
+      v-model:playerName="playerNames[6]"
+    />
+    <CharacterSelect
+      playerNumber="8"
+      v-model:selectedCharacter="characters[7]"
+      v-model:playerName="playerNames[7]"
+    />
   </div>
+  <p>DEBUG: Characters selected: {{ characters }}</p>
+  <p>DEBUG: Player names: {{ playerNames }}</p>
   <div>
-    <OutputScreen />
+    <OutputScreen :playerNames="playerNames" :characters="characters" />
   </div>
 </template>
--- src/components/OutputScreen.vue
+++ src/components/OutputScreen.vue
@@ -2,18 +2,39 @@
 import { useTemplateRef } from 'vue';
 import exportAsImage from '../utils/exportAsImage';
 const input = useTemplateRef('exportRef');
+const props = defineProps<{ characters: string[]; playerNames: string[] }>();
+
+function getImagePath(character: string | undefined): string {
+  return `/src/assets/character-portraits/${character}/Default.png`;
+}
 </script>
 
 <template>
   <div ref="exportRef" class="canvas">
-    <div class="character-frame" id="frame1"></div>
-    <div class="character-frame" id="frame2"></div>
-    <div class="character-frame" id="frame3"></div>
-    <div class="character-frame" id="frame4"></div>
-    <div class="character-frame" id="frame5"></div>
-    <div class="character-frame" id="frame6"></div>
-    <div class="character-frame" id="frame7"></div>
-    <div class="character-frame" id="frame8"></div>
+    <div class="character-frame" id="frame1">
+      <img :src="getImagePath(props.characters[0])" class="character-image" />
+    </div>
+    <div class="character-frame" id="frame2">
+      <img :src="getImagePath(props.characters[1])" class="character-image" />
+    </div>
+    <div class="character-frame" id="frame3">
+      <img :src="getImagePath(props.characters[2])" class="character-image" />
+    </div>
+    <div class="character-frame" id="frame4">
+      <img :src="getImagePath(props.characters[3])" class="character-image" />
+    </div>
+    <div class="character-frame" id="frame5">
+      <img :src="getImagePath(props.characters[4])" class="character-image" />
+    </div>
+    <div class="character-frame" id="frame6">
+      <img :src="getImagePath(props.characters[5])" class="character-image" />
+    </div>
+    <div class="character-frame" id="frame7">
+      <img :src="getImagePath(props.characters[6])" class="character-image" />
+    </div>
+    <div class="character-frame" id="frame8">
+      <img :src="getImagePath(props.characters[7])" class="character-image" />
+    </div>
   </div>
   <button class="btn" @click="exportAsImage(input, 'test-image')">Export as Image</button>
 </template>
