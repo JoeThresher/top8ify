bef9ade4a96cb993c8ffee7ac342bfc89b73e315
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-11-11 21:12:54 -0500

    Minor formatting changes

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -19,63 +19,67 @@ async function fetchStartGGData() {
 </script>
 
 <template>
-  <input type="text" placeholder="Enter Start GG URL here" class="input" v-model="url" />
-  <input type="text" placeholder="Start GG Token" class="input" v-model="token" />
-  <button class="btn" @click="fetchStartGGData()">Fetch StartGG Data</button>
-  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
-    <CharacterSelect
-      playerNumber="1"
-      v-model:selectedCharacter="characters[0]"
-      v-model:playerName="playerNames[0]"
-      v-model:characterColor="characterColors[0]"
-    />
-    <CharacterSelect
-      playerNumber="2"
-      v-model:selectedCharacter="characters[1]"
-      v-model:playerName="playerNames[1]"
-      v-model:characterColor="characterColors[1]"
-    />
-    <CharacterSelect
-      playerNumber="3"
-      v-model:selectedCharacter="characters[2]"
-      v-model:playerName="playerNames[2]"
-      v-model:characterColor="characterColors[2]"
-    />
-    <CharacterSelect
-      playerNumber="4"
-      v-model:selectedCharacter="characters[3]"
-      v-model:playerName="playerNames[3]"
-      v-model:characterColor="characterColors[3]"
-    />
-    <CharacterSelect
-      playerNumber="5"
-      v-model:selectedCharacter="characters[4]"
-      v-model:playerName="playerNames[4]"
-      v-model:characterColor="characterColors[4]"
-    />
-    <CharacterSelect
-      playerNumber="6"
-      v-model:selectedCharacter="characters[5]"
-      v-model:playerName="playerNames[5]"
-      v-model:characterColor="characterColors[5]"
-    />
-    <CharacterSelect
-      playerNumber="7"
-      v-model:selectedCharacter="characters[6]"
-      v-model:playerName="playerNames[6]"
-      v-model:characterColor="characterColors[6]"
-    />
-    <CharacterSelect
-      playerNumber="8"
-      v-model:selectedCharacter="characters[7]"
-      v-model:playerName="playerNames[7]"
-      v-model:characterColor="characterColors[7]"
-    />
+  <div class="p-4 space-y-4 m-4 grid place-items-center">
+    <p>Start GG Import</p>
+    <input type="text" placeholder="Enter Start GG URL here" class="input" v-model="url" />
+    <input type="text" placeholder="Start GG Token" class="input" v-model="token" />
+    <button class="btn" @click="fetchStartGGData()">Fetch StartGG Data</button>
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4 gap-4">
+      <CharacterSelect
+        playerNumber="1"
+        v-model:selectedCharacter="characters[0]"
+        v-model:playerName="playerNames[0]"
+        v-model:characterColor="characterColors[0]"
+      />
+      <CharacterSelect
+        playerNumber="2"
+        v-model:selectedCharacter="characters[1]"
+        v-model:playerName="playerNames[1]"
+        v-model:characterColor="characterColors[1]"
+      />
+      <CharacterSelect
+        playerNumber="3"
+        v-model:selectedCharacter="characters[2]"
+        v-model:playerName="playerNames[2]"
+        v-model:characterColor="characterColors[2]"
+      />
+      <CharacterSelect
+        playerNumber="4"
+        v-model:selectedCharacter="characters[3]"
+        v-model:playerName="playerNames[3]"
+        v-model:characterColor="characterColors[3]"
+      />
+      <CharacterSelect
+        playerNumber="5"
+        v-model:selectedCharacter="characters[4]"
+        v-model:playerName="playerNames[4]"
+        v-model:characterColor="characterColors[4]"
+      />
+      <CharacterSelect
+        playerNumber="6"
+        v-model:selectedCharacter="characters[5]"
+        v-model:playerName="playerNames[5]"
+        v-model:characterColor="characterColors[5]"
+      />
+      <CharacterSelect
+        playerNumber="7"
+        v-model:selectedCharacter="characters[6]"
+        v-model:playerName="playerNames[6]"
+        v-model:characterColor="characterColors[6]"
+      />
+      <CharacterSelect
+        playerNumber="8"
+        v-model:selectedCharacter="characters[7]"
+        v-model:playerName="playerNames[7]"
+        v-model:characterColor="characterColors[7]"
+      />
+    </div>
   </div>
+
   <p>DEBUG: Characters selected: {{ characters }}</p>
   <p>DEBUG: Player names: {{ playerNames }}</p>
   <p>DEBUG: Character colors: {{ characterColors }}</p>
-  <div>
+  <div class="grid place-items-center">
     <OutputScreen
       :playerNames="playerNames"
       :characters="characters"
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import MainScreen from '../components/MainScreen.vue'
+import MainScreen from '../components/MainScreen.vue';
+import NavBar from '../components/NavBar.vue';
 </script>
 
 <template>
   <main>
+    <NavBar />
     <MainScreen />
   </main>
 </template>
