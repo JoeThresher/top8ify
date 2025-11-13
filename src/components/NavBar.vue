9f05a92c95ff3f4061ae69e5941ffa60e15b66e2
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-11-12 20:58:40 -0500

    Added settings button to navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,26 @@
+<script setup lang="ts">
+import { Menu } from 'lucide-vue-next';
+</script>
+
 <template>
   <div class="navbar bg-base-100 shadow-sm">
-    <a class="btn btn-ghost text-xl">Top8ify</a>
+    <div class="flex-1">
+      <a class="btn btn-ghost text-xl">Top8ify</a>
+    </div>
+    <div class="flex-none">
+      <div class="dropdown dropdown-end">
+        <div tabindex="0" role="button" class="btn btn-ghost w-8 mr-4">
+          <div class="w-10 rounded-full">
+            <Menu />
+          </div>
+        </div>
+        <ul
+          tabindex="-1"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+        >
+          <li><a>Settings</a></li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
