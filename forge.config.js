61e6857e11e651659ba363077c9603944cea011a
JoeThresher <71785144+JoeThresher@users.noreply.github.com>
2025-12-06 16:56:19 -0500

    Updated version number and changed capitalization of output app

--- forge.config.js
+++ forge.config.js
@@ -4,6 +4,8 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 module.exports = {
   packagerConfig: {
     asar: true,
+    name: 'Top8ify',
+    executableName: 'top8ify',
   },
   rebuildConfig: {},
   makers: [
